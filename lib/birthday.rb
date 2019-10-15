7e679245895c8773f1e9cdd842d73f6522a40b68
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-15 20:46:54 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,11 +13,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age <= 12 
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
-    end
+    age <= 12 ? (puts "Happy Birthday #{kids_name}! You are now #{age} years old!") : (puts "You are too old for this.") #replaced if/else statement with ternary operator
   end
 end
-
